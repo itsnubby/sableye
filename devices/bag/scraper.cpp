30382a9574e1a435f8c109308dd46531a133b51a
nubby <nubby@funkle.milk.net>
2020-04-07 14:40:24 -0700

    work done on them devices...or at least cameras

---
+++ devices/bag/scraper.cpp
@@ -0,0 +1,12 @@
+/* scrape_cams.cpp - Help find all connected cameras.
+ * modified: 4/7/2020
+ *  ) 0 o .
+ */
+#include <sys/ioctl.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hi nub.\n";
+  return 0;
+}
